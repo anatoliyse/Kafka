43a78e6a8cd5b8df0a6cfbf36a2aac34addaf609
anatoliyse <109183055+anatoliyse@users.noreply.github.com>
2022-10-08 15:09:13 +0300

    Add files via upload

---
+++ LAB1/producer.py
@@ -0,0 +1,26 @@
+try:
+    from kafka import KafkaProducer
+    from faker import Faker
+    import json
+    from time import sleep
+except Exception as e:
+    pass
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+_instance = Faker()
+
+
+for _ in range(20):
+    _data = {
+        "first_name": _instance.first_name(),
+        "city":_instance.city(),
+        "phone_number":_instance.phone_number(),
+        "state":_instance.state(),
+        "id":str(_)
+    }
+    _payload = json.dumps(_data).encode("utf-8")
+    response = producer.send('FirstTopic', _payload)
+    print(response)
+
+    sleep(2)
+
